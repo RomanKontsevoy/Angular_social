583e68007111b0a8715d7742363870b613ff4ba2
Roman <1604881@gmail.com>
2017-07-20 01:29:13 +0300

    'Welcome' is displayed at the right of the header'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,22 @@ import { UsersComponent } from './users/users.component';
 import { HeaderComponent } from './header/header.component';
 import { UsersService } from './_services/users.service';
 import { LikeComponent } from './like/like.component';
+import { PostComponent } from './post/post.component';
+import { CommentsComponent } from './comments/comments.component';
+// import { AlertModule } from 'bootstrap';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     HeaderComponent,
-    LikeComponent
+    LikeComponent,
+    PostComponent,
+    CommentsComponent
   ],
   imports: [
+    // AlertModule.forRoot(),
     BrowserModule,
     HttpModule,
     RouterModule.forRoot([
@@ -23,7 +30,7 @@ import { LikeComponent } from './like/like.component';
         path: 'users',
         component: UsersComponent
       },
-    ])
+    ]),
   ],
   providers: [UsersService],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../_services/users.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'header',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  id;
+  user;
+    constructor(
+      private us: UsersService,
+      private router: ActivatedRoute
+
+    ) { }
+
+
+  // ngOnInit() {
+  //   this.us.getUser(3)
+  //     .then( res => {
+  //       this.user = res.json();
+  //     } )
+  //     .catch( error => {
+  //       console.log( error );
+  //     } )
+  // }
 
   ngOnInit() {
+    // this.getId();
+    this.us.getUser( 2 )
+      .then( res => {
+        this.user = res.json();
+      } )
+      .catch( error => {
+        console.log( error );
+      } )
   }
+
+  // getId(){
+  //   this.router.params.subscribe( route => {
+  //     this.id = route['id'];
+  //   } )
+  // }
 
 }
