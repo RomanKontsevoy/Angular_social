60aa35de87b048f86fdab63c7bdee2831fb84c1c
Roman <1604881@gmail.com>
2017-07-20 23:29:07 +0300

    have no time to finish posts

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from "../_services/post.service";
+// import {  } from "@angular/http";
+import { PostsComponent } from '../../../../angularApp_38/src/app/posts/posts.component';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.sass']
+  styleUrls: ['./post.component.sass'],
+  providers: [PostService]
 })
-export class PostComponent implements OnInit {
 
-  constructor() { }
+export class PostComponent implements OnInit {
+  posts = [];
+
+  isLoading = true;
+  constructor(private ps:PostService) {
+   };
 
   ngOnInit() {
+    this.ps.getPosts()
+    .then(posts => {
+      this.isLoading = false;
+      console.log(posts[0].title);
+    });
   }
 
 }
